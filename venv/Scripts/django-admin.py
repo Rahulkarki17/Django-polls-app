9dc3cdf259c62b768aa6400f0907103bd83bde49
Rahulkarki17 <rahulkarki@hotmail.com>
2018-11-02 11:58:39 +0000

    First commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\richa\documents\rahul_projects\programming\web\django_polls\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
